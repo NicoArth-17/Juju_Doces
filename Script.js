12fda8bdf898e700083f2224a14800815c09b4a8
Nicolas Arthur <nicolarthur17@hotmail.com>
2022-03-30 21:33:33 -0300

    landing page de doces em andamento

---
+++ Script.js
@@ -0,0 +1,11 @@
+function AbrirInsta(){
+    window.open('https://www.instagram.com/juju.d0ces/')
+}
+
+function AbrirWpp(){
+    window.open('https://api.whatsapp.com/send?phone=5531982961127')
+}
+
+function AbrirIfood(){
+    window.open('https://www.ifood.com.br/delivery/belo-horizonte-mg/juju-doces-aarao-reis/6570d90a-e33d-4421-a3a9-3b689952c8ab')
+}
