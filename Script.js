400c895de9a50633c16bc41bb706306effcd1262
Nicolas Arthur <nicolarthur17@hotmail.com>
2022-04-22 22:05:13 -0300

    site desktop finalizado, proximo passo responsividade

--- Script.js
+++ Script.js
@@ -9,3 +9,9 @@ function AbrirWpp(){
 function AbrirIfood(){
     window.open('https://www.ifood.com.br/delivery/belo-horizonte-mg/juju-doces-aarao-reis/6570d90a-e33d-4421-a3a9-3b689952c8ab')
 }
+
+function ClickMenu(){
+    document.getElementById("menu").classList.toggle("change")
+    document.getElementById("nav").classList.toggle("change")
+    document.getElementById("menu-bg").classList.toggle("change-bg")
+}
